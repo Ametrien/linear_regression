ef59c039c044e6ffd93fb7dda13bbcc9a4d7fe93
Ametrien <anastasia.ivanova@ati.utm.md>
2021-02-21 10:21:30 +0200

    User input added

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from sklearn.utils.extmath import safe_sparse_dot
+
 import data_remover as dr
+import pandas as pd
+import numpy as np
+
 from sklearn.model_selection import train_test_split
-import pandas as pd  # To read data
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from scipy.stats import pearsonr, chi2_contingency, ttest_ind
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 dr.data_remover()
 # Load data set
@@ -26,7 +32,11 @@ regr.fit(X_train, Y_train)  # performs linear regression
 Y_pred = regr.predict(X_test)  # makes predictions
 
 # The coefficients
-print('Coefficients: \n', regr.coef_)
+print('In multiple regression each coefficient is interpreted as the estimated change in y\n'
+      'corresponding to a one unit change in a variable, when all other variables are held constant\n'
+      'Slope coefficients: \n', regr.coef_)
+
+print('Intercept: \n', regr.intercept_)
 # The mean squared error
 print('Mean absolute error: %.2f'
       % mean_absolute_error(Y_test, Y_pred))
@@ -37,36 +47,54 @@ print('Mean squared error: %.2f'
 print('Coefficient of determination: %.2f\n'
       % r2_score(Y_test, Y_pred))
 
-# Pearson’s Correlation Coefficient
-# tests whether two samples have a linear relationship
+# Estimated multiple linear regression equation
+# 𝑓(𝑥) = 𝑏₀ + 𝑏₁𝑥₁ + 𝑏₂𝑥₂ + 𝑏3𝑥3 + 𝑏4𝑥4 + b5x5
 
-data1 = Y_test
-data2 = Y_pred
-stat, p = pearsonr(data1, data2)
-print('Pearson’s Correlation Coefficient. stat=%.3f, p=%.3f' % (stat, p))
-if p > 0.05:
-    print('Probably independent\n')
-else:
-    print('Probably dependent\n')
+x1 = input("Enter complexAge\n")
+x2 = input("Enter totalRooms\n")
+x3 = input("Enter totalBedrooms\n")
+x4 = input("Enter complexInhabitants\n")
+x5 = input("Enter apartmentsNr\n")
 
-# Chi-Squared Test
-# tests whether two categorical variables are related or independent
+summ = 0
+inputs = [x1, x2, x3, x4, x5]
+for i in range (0, 5):
+    for x in inputs:
+       summ += regr.coef_[i] * int(x)
 
-table = [data['totalRooms'], data['medianComplexValue']]
-stat, p, dof, expected = chi2_contingency(table)
-print('Chi-Squared Test. stat=%.3f, p=%.3f' % (stat, p))
-if p > 0.05:
-    print('Probably independent\n')
-else:
-    print('Probably dependent\n')
+result = regr.intercept_ + summ
+print("The medianComplexValue is", int(result))
 
-# Student’s t-test
-# tests whether the means of two independent samples are significantly different
-data1 = Y_test
-data2 = Y_pred
-stat, p = ttest_ind(data1, data2)
-print('Student’s t-test. stat=%.3f, p=%.3f' % (stat, p))
-if p > 0.05:
-    print('Probably the same distribution\n')
-else:
-    print('Probably different distributions\n')
+# # Pearson’s Correlation Coefficient
+# # tests whether two samples have a linear relationship
+#
+# data1 = Y_test
+# data2 = Y_pred
+# stat, p = pearsonr(data1, data2)
+# print('Pearson’s Correlation Coefficient. stat=%.3f, p=%.3f' % (stat, p))
+# if p > 0.05:
+#     print('Probably independent\n')
+# else:
+#     print('Probably dependent\n')
+#
+# # Chi-Squared Test
+# # tests whether two categorical variables are related or independent
+#
+# table = [data['apartmentsNr'], data['medianComplexValue']]
+# stat, p, dof, expected = chi2_contingency(table)
+# print('Chi-Squared Test. stat=%.3f, p=%.3f' % (stat, p))
+# if p > 0.05:
+#     print('Probably independent\n')
+# else:
+#     print('Probably dependent\n')
+#
+# # Student’s t-test
+# # tests whether the means of two independent samples are significantly different
+# data1 = Y_test
+# data2 = Y_pred
+# stat, p = ttest_ind(data1, data2)
+# print('Student’s t-test. stat=%.3f, p=%.3f' % (stat, p))
+# if p > 0.05:
+#     print('Probably the same distribution\n')
+# else:
+#     print('Probably different distributions\n')
