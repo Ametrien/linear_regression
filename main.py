1ba051704ac25dea580465392ee7b6caf253993b
Ametrien <anastasia.ivanova@ati.utm.md>
2021-02-22 14:42:35 +0200

    Final version

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from sklearn.preprocessing import Normalizer
-
 import data_remover as dr
 import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn import preprocessing
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.metrics import r2_score
 from scipy.stats import pearsonr, chi2_contingency, ttest_ind
 import sklearn.metrics as sm
 import seaborn as sns
@@ -21,9 +18,6 @@ data = pd.read_csv('dataset.csv')
 data.columns = ['complexAge', 'totalRooms', 'totalBedrooms',
                 'complexInhabitants', 'apartmentsNr', 'medianComplexValue']
 
-# Normalization that either does not work or makes no sense at all
-# Normalizer().fit_transform(data)
-
 # Independent variables are stored in X
 X = data[['complexAge', 'totalRooms', 'totalBedrooms',
           'complexInhabitants', 'apartmentsNr']].values
@@ -32,11 +26,6 @@ Y = data['medianComplexValue'].values  # values converts it into a numpy array
 
 # Split the data into training/testing sets
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
-
-# Heatmap. You want the red line for y=x because values should be correlated with themselves. However, any red or blue
-# columns show there's a strong correlation/anti-correlation that requires more investigation. For example,
-# Resource=1, Resource=4, might be highly correlated in the sense if people have 1 there is a less chance to have 4,
-# etc. Regression assumes that the parameters used are independent from one another.
 
 corr = pd.DataFrame(data).corr()
 sns.heatmap(corr, mask=np.zeros_like(corr, dtype=bool),
@@ -48,12 +37,8 @@ clf = regr.fit(X_train, Y_train)  # performs linear regression
 Y_pred = regr.predict(X_test)  # makes predict
 print('This is it', Y_pred)
 
-# ions
-
 # The coefficients
-print('In multiple regression each coefficient is interpreted as the estimated change in y\n'
-      'corresponding to a one unit change in a variable, when all other variables are held constant\n'
-      'Slope coefficients: \n', regr.coef_)
+print('Slope coefficients: \n', regr.coef_)
 
 print('Intercept: \n', regr.intercept_)
 # The mean squared error
@@ -66,12 +51,44 @@ print("Relative mean squared error =", round(sm.mean_squared_error(Y_test, Y_pre
 print("Median absolute error =", round(sm.median_absolute_error(Y_test, Y_pred), 2))
 print("Explain variance score =", round(sm.explained_variance_score(Y_test, Y_pred), 2))
 print("Max error =", round(sm.max_error(Y_test, Y_pred), 2))
-
 print("R2 score =", round(sm.r2_score(Y_test, Y_pred), 2))
+
+# Pearson’s Correlation Coefficient
+# tests whether two samples have a linear relationship
+
+data1 = Y_test
+data2 = Y_pred
+stat, p = pearsonr(data1, data2)
+print('\nPearson’s Correlation Coefficient. stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+    print('Probably independent\n')
+else:
+    print('Probably dependent\n')
+
+# Chi-Squared Test
+# tests whether two categorical variables are related or independent
+
+table = [data['apartmentsNr'], data['medianComplexValue']]
+stat, p, dof, expected = chi2_contingency(table)
+print('Chi-Squared Test. stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+    print('Probably independent\n')
+else:
+    print('Probably dependent\n')
+
+# Student’s t-test
+# tests whether the means of two independent samples are significantly different
+data1 = Y_test
+data2 = Y_pred
+stat, p = ttest_ind(data1, data2)
+print('Student’s t-test. stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+    print('Probably the same distribution\n')
+else:
+    print('Probably different distributions\n')
 
 # Estimated multiple linear regression equation
 # 𝑓(𝑥) = 𝑏₀ + 𝑏₁𝑥₁ + 𝑏₂𝑥₂ + 𝑏3𝑥3 + 𝑏4𝑥4 + b5x5
-
 
 # x1 = input("Enter complexAge\n")
 # x2 = input("Enter totalRooms\n")
@@ -84,37 +101,3 @@ inputs = [[1, 7099, 1106, 2401, 1138]]  # test is 358500, predicted is 317416
 prediction = regr.predict(inputs).flatten()
 
 print("Predicted value", prediction)
-
-# # Pearson’s Correlation Coefficient
-# # tests whether two samples have a linear relationship
-#
-# data1 = Y_test
-# data2 = Y_pred
-# stat, p = pearsonr(data1, data2)
-# print('Pearson’s Correlation Coefficient. stat=%.3f, p=%.3f' % (stat, p))
-# if p > 0.05:
-#     print('Probably independent\n')
-# else:
-#     print('Probably dependent\n')
-#
-# # Chi-Squared Test
-# # tests whether two categorical variables are related or independent
-#
-# table = [data['apartmentsNr'], data['medianComplexValue']]
-# stat, p, dof, expected = chi2_contingency(table)
-# print('Chi-Squared Test. stat=%.3f, p=%.3f' % (stat, p))
-# if p > 0.05:
-#     print('Probably independent\n')
-# else:
-#     print('Probably dependent\n')
-#
-# # Student’s t-test
-# # tests whether the means of two independent samples are significantly different
-# data1 = Y_test
-# data2 = Y_pred
-# stat, p = ttest_ind(data1, data2)
-# print('Student’s t-test. stat=%.3f, p=%.3f' % (stat, p))
-# if p > 0.05:
-#     print('Probably the same distribution\n')
-# else:
-#     print('Probably different distributions\n')
